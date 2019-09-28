0cbbf6200a7bd75e36ac5ad0feeb61a1f2f9c03f
katomasahiro10 <katomasahiro10@gmail.com>
2019-09-28 16:30:26 +0900

    Neuron, Connection classの定義を分けた

---
+++ neuron.py
@@ -0,0 +1,55 @@
+import random
+import math
+from enum import Enum
+from const import *
+
+class NeuronType(Enum):
+    INPUT = 1
+    OUTPUT = 2
+    NORMAL = 3
+    MODULATION = 4
+    
+class Neuron:
+    def __init__(self, neuron_type:NeuronType, neuron_id):
+        self.neuron_id = neuron_id
+        self.neuron_type = neuron_type
+        self.bias = random.uniform(BIAS_LOWER_LIMIT,BIAS_UPPER_LIMIT)
+        self.__activation = 0.0
+        self.__modulation = 0.0
+
+    @property
+    def activation(self):
+        if(self.neuron_type == NeuronType.MODULATION):
+            return 0.0
+        else:
+            return self.__activation
+
+    @activation.setter
+    def activation(self,activation_val):
+        if(self.neuron_type == NeuronType.MODULATION):
+            raise Exception('ERROR: you cannot set activation val of modulation neuron')
+        else:
+            self.__activation = activation_val
+
+    @property
+    def modulation(self):
+        if(self.neuron_type == NeuronType.MODULATION):
+            return self.__modulation
+        else:
+            return 0.0
+
+    @modulation.setter
+    def modulation(self,modulation_val):
+        if(self.neuron_type == NeuronType.MODULATION):
+            self.__modulation = modulation_val
+        else:
+            raise Exception('ERROR: you cannot set modulation val of not modulation neuron')
+
+class Connetion:
+    def __init__(self, connection_id, input_id, output_id):
+        self.connection_id = connection_id
+        self.is_valid = True
+        self.weight = random.uniform(WEIGHT_LOWER_LIMIT,WEIGHT_UPPER_LIMIT)
+        self.initial_weight = self.weight
+        self.input_id = input_id
+        self.output_id = output_id
