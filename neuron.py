736fee7cfb2c9d8fb83648ff0de1c52d50ed9b9c
katomasahiro10 <katomasahiro10@gmail.com>
2019-09-28 11:15:52 +0900

    commit const, neuron

---
+++ neuron.py
@@ -0,0 +1,27 @@
+import random
+import math
+from enum import Enum
+
+class NeuronType(Enum):
+    INPUT = 1
+    OUTPUT = 2
+    HIDDEN = 3
+    MODULATION = 4
+    
+class Neuron:
+    def __init__(self, neuron_type:NeuronType):
+        self.neuron_type = neuron_type
+        self.bias = bias
+        self.normal_input_sum = 0
+        self.modulation_input_sum = 0
+        self.activation = 0.0
+
+class Connetion:
+    def __init__(self):
+        self.innovation_no =
+        self.is_able = True
+        self.initial_weight = 
+        self.weight = initial_weight
+        self.input_no =
+        self.output_no = 
+
