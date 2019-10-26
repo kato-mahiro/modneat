abac6cd7adbd1f140d6a3f9275e05a05b9362078
katomasahiro10 <katomasahiro10@gmail.com>
2019-10-27 01:53:37 +0900

    xor.pyを実装

--- xor.py
+++ xor.py
@@ -1,28 +1,40 @@
 #usage sample
 
+import random
+
 import agents
 
+# target inputs and outputs
 inputs = [[0,0],[1,0],[0,1],[1,1]]
 outputs = [0,1,1,0]
 
-agents = agents.Agents('NeuralNetwork',10)
+# define populations
+a = agents.Agents('NeuralNetwork',10)
 
-for g in range(4000):
-    average_fitness=0
+# show individual
+print(a[0].get_output_without_update(inputs[0]))
+print(a[0].get_output_without_update(inputs[1]))
+print(a[0].get_output_without_update(inputs[2]))
+print(a[0].get_output_without_update(inputs[3]))
+a[0].show_network('first_individual')
+
+for g in range(5000):
     for n in range(10):
         error = 0.0
-        error += (agents[n].get_output_without_update(inputs[0])[0] - outputs[0]) ** 2
-        error += (agents[n].get_output_without_update(inputs[1])[0] - outputs[1]) ** 2
-        error += (agents[n].get_output_without_update(inputs[2])[0] - outputs[2]) ** 2
-        error += (agents[n].get_output_without_update(inputs[3])[0] - outputs[3]) ** 2
-        agents[n].fitness = error *-1
-        average_fitness += agents[n].fitness
-        #print(agents[n].fitness)
-    if(g%10 == 0):
-        print(average_fitness/10)
-    agents = agents.evolution(elite_num = 2, mutate_prob = 0.01, sigma = 0.1)
+        error += (a[n].get_output_without_update(inputs[0])[0] - outputs[0]) ** 2
+        error += (a[n].get_output_without_update(inputs[1])[0] - outputs[1]) ** 2
+        error += (a[n].get_output_without_update(inputs[2])[0] - outputs[2]) ** 2
+        error += (a[n].get_output_without_update(inputs[3])[0] - outputs[3]) ** 2
+        a[n].fitness = error * -1
+    if(g % 100 == 0):
+        print(g, a.average_fitness )
 
-print(agents[0].get_output_without_update(inputs[0]))
-print(agents[0].get_output_without_update(inputs[1]))
-print(agents[0].get_output_without_update(inputs[2]))
-print(agents[0].get_output_without_update(inputs[3]))
+    # evolution
+    a = a.evolution(elite_num = 2, mutate_prob = 0.1, sigma = 0.1)
+
+# show last individual
+print(a[0].get_output_without_update(inputs[0]))
+print(a[0].get_output_without_update(inputs[1]))
+print(a[0].get_output_without_update(inputs[2]))
+print(a[0].get_output_without_update(inputs[3]))
+a[0].show_network('best_individual')
