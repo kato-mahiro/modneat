429e35b39bdd6cf0c11288f72eb33bcf45eddca1
katomasahiro10 <katomasahiro10@gmail.com>
2020-02-14 18:32:34 +0900

    Squashed commit of the following:
    commit 982b7d47bf4c877646bbfb470b208fa2441f2347
    Author: katomasahiro10 <katomasahiro10@gmail.com>
    Date:   Fri Feb 14 18:31:45 2020 +0900
    
        implement evolution_mgg function
    
        xor.pyもうまく進化した
    
    commit 12f89207ba39763d796ba90e4d84571670d618f8
    Author: katomasahiro10 <katomasahiro10@gmail.com>
    Date:   Fri Feb 14 15:22:00 2020 +0900
    
        edit agents.py
    
        新しい進化方法 evolution_mgg関数を実装した

--- modneat/agents.py
+++ modneat/agents.py
@@ -115,6 +115,66 @@ class Agents(list):
         #引数で返すようにしないとなぜか内容が更新されない
         return next_agents
 
+    def evolution_mgg(self, task, elite_num = 0, mutate_prob=0.01, sigma=0.1): # task(): function, which take an agent as argument and return fitness
+        self.sort(key=attrgetter('fitness'), reverse = True)
+
+        next_agents = copy.deepcopy(self)
+        next_agents.clear()
+        for i in range(elite_num):
+            elite_agent = copy.deepcopy(self[i])
+            elite_agent.reset()
+            next_agents.append(elite_agent)
+
+        # evolution
+        if(self.agent_num - elite_num%2 == 1):
+            raise Exception('Error: (evolution_mgg) agent_num - elite_num must be even number')
+        for i in range( (self.agent_num - elite_num) // 2):
+            four_agents = []
+            larger = lambda a,b: a if a>b else b
+
+            parent = random.choice(self)
+            parent_A = copy.deepcopy(parent)
+            four_agents.append(parent_A)
+
+            parent = random.choice(self)
+            parent_B = copy.deepcopy(parent)
+            four_agents.append(parent_B)
+
+            new_agent_a = crossover(parent_A, parent_A.fitness, parent_B, parent_B.fitness)
+            new_agent_a = mutate_add_connection(new_agent_a,larger(self.global_max_connection_id,next_agents.global_max_connection_id)) if random.random() < mutate_prob else new_agent_a
+            new_agent_a = mutate_disable_connection(new_agent_a) if random.random() < mutate_prob else new_agent_a
+            new_agent_a = mutate_add_neuron(new_agent_a, larger(self.global_max_connection_id, next_agents.global_max_connection_id)) if random.random() < mutate_prob else new_agent_a
+            new_agent_a = give_dispersion(new_agent_a, rate=mutate_prob, sigma=sigma)
+            new_agent_a.fitness = task(new_agent_a)
+            four_agents.append(new_agent_a)
+
+            new_agent_b = crossover(parent_A, parent_A.fitness, parent_B, parent_B.fitness)
+            new_agent_b = mutate_add_connection(new_agent_b,larger(self.global_max_connection_id,next_agents.global_max_connection_id)) if random.random() < mutate_prob else new_agent_b
+            new_agent_b = mutate_disable_connection(new_agent_b) if random.random() < mutate_prob else new_agent_b
+            new_agent_b = mutate_add_neuron(new_agent_b, larger(self.global_max_connection_id, next_agents.global_max_connection_id)) if random.random() < mutate_prob else new_agent_b
+            new_agent_b = give_dispersion(new_agent_b, rate=mutate_prob, sigma=sigma)
+            new_agent_b.fitness = task(new_agent_b)
+            four_agents.append(new_agent_b)
+
+            four_agents.sort(key=attrgetter('fitness'), reverse = True)
+            best_offspring = four_agents[0]
+
+            fitness_list = [ four_agents[i].fitness for i in range(4) ]
+            min_fitness = min(fitness_list)
+            max_fitness = max(fitness_list)
+            range_fitness = max_fitness - min_fitness +0.001
+            fitness_list = [(fitness_list[i] - min_fitness)/range_fitness +0.001 for i in range(len(fitness_list))]
+            roulette_offspring = random.choices(four_agents, weights=fitness_list)[0]
+
+            next_agents.append(best_offspring)
+            next_agents.append(roulette_offspring)
+
+        for i in range(len(next_agents)):
+            next_agents[i].reset()
+
+        #引数で返すようにしないとなぜか内容が更新されない
+        return next_agents
+
     def save_agents(self,name='undefined.pickle'):
         f = open(name,'wb')
         pickle.dump(self,f)
